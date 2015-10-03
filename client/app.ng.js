b56506303d37cb1ea00bee237726cef7b14733cb
kraftingas <r.paresciute@gmail.com>
2015-10-03 13:27:37 +0300

    Create PostsList controller
    * get data of posts
    * show posts in a list

--- client/app.ng.js
+++ client/app.ng.js
@@ -1,1 +1,5 @@
 angular.module('meteor-sample', ['angular-meteor']);
+
+angular.module('meteor-sample').controller('PostsListCtrl', ['$scope', '$meteor', function($scope, $meteor) {
+  $scope.posts = $meteor.collection(Posts);
+}]);
