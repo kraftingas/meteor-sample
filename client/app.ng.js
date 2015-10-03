47b5ca2c4a5677d3af90e0ce9f0fd14da28e8b3d
kraftingas <r.paresciute@gmail.com>
2015-10-03 13:40:50 +0300

    Control posts
    * add new posts
    * delete post
    * delete all posts

--- client/app.ng.js
+++ client/app.ng.js
@@ -2,4 +2,16 @@ angular.module('meteor-sample', ['angular-meteor']);
 
 angular.module('meteor-sample').controller('PostsListCtrl', ['$scope', '$meteor', function($scope, $meteor) {
   $scope.posts = $meteor.collection(Posts);
+
+  $scope.save = function(post) {
+    $scope.posts.save(post);
+  };
+
+  $scope.remove = function(post) {
+    $scope.posts.remove(post);
+  };
+
+  $scope.removeAll = function() {
+    $scope.posts.remove();
+  };
 }]);
